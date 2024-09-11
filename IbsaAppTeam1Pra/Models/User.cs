7ca45f2328ba5f6b45f972b1f127de41baa3f504
ivargek23 <128840564+ivargek23@users.noreply.github.com>
2024-09-11 23:49:46 +0200

    - added bankar role - saved transactions for bankar to see

--- IbsaAppTeam1Pra/Models/User.cs
+++ IbsaAppTeam1Pra/Models/User.cs
@@ -2,14 +2,14 @@
 {
     public class User
     {
-        public string FirstName { get; set; }
-        public string LastName { get; set; }
-        public string Email { get; set; }
+        public string FirstName { get; set; } = "John";
+        public string LastName { get; set; } = "Doe";
+        public string Email { get; set; } = "default@email.hr";
         public string Password { get; set; }
         public string Username { get; set; }
-        public string Oib { get; set; }
-        public Account AccountOne { get; set; }
-        public Account AccountTwo { get; set; }
+        public string Oib { get; set; } = "12345678901";
+        public Account AccountOne { get; set; } = null;
+        public Account AccountTwo { get; set; } = null;
 
         public User()
         {
